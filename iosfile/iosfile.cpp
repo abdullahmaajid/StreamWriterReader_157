c2d9cf35fefd1197e1f16a5b8080aca56627204b
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-06-19 15:00:54 +0700

    // tutup file tersebut setelah selesai   infile.close();

--- iosfile/iosfile.cpp
+++ iosfile/iosfile.cpp
@@ -39,9 +39,12 @@ int main() {
 	if (infile.is_open())
 	{
 		// melakukan perulangan setiap baris
-		while (getline(infile, baris)) {
+		while (getline(infile, baris)) 
+		{
 			// dan tampilkan disini
 			cout << baris << '\n\';
 		}
+		// tutup file tersebut setelah selesai
+		infile.close();
 	}
 }
